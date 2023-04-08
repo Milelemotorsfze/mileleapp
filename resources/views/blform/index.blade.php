d4247eeba62292d6139857bc992e97cc9426cbec
Phoji17 <muhammad.qasim@milele.com>
2023-04-08 12:19:37 +0400

    BL Form
    BL form has been created, along with the database file.

--- resources/views/blform/index.blade.php
+++ resources/views/blform/index.blade.php
@@ -4,22 +4,16 @@
     <h4 class="card-title">
       Bill of Lading Form
     </h4>
-    @can('blfrom-create')
-      <a style="float: right;" class="btn btn-sm btn-success" href="{{ route('blfrom.create') }}" text-align: right>
-        <i class="fa fa-plus" aria-hidden="true"></i> Add New BL Form
-      </a>
-    @endcan
     <ul class="nav nav-pills nav-fill">
       <li class="nav-item">
-        <a class="nav-link active" data-bs-toggle="pill" href="#tab1">New / Pending Calls to Leads</a>
+        <a class="nav-link active" data-bs-toggle="pill" href="#tab1">Bill of Lading Form</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="pill" href="#tab2">Calls to Leads Converted</a>
+        <a class="nav-link" data-bs-toggle="pill" href="#tab2">All VINs of BL</a>
       </li>
     </ul>      
   </div>
   <div class="tab-content">
-    @can('blfrom-list')
       <div class="tab-pane fade show active" id="tab1"> 
         <div class="card-body">
           <div class="table-responsive">
@@ -30,19 +24,13 @@
                 </tr>
               </thead>
               <tbody>
-              <div hidden>{{$i=0;}}</div>
-                @foreach ($data as $key => $blfrom)
-                  <tr data-id="1">
-                    <td>{{ ++$i }}</td>
-                  </tr>
-                @endforeach
+                <a style="float: right;" class="btn btn-sm btn-success" href="{{ route('blfrom.create') }}" text-align: right><i class="fa fa-plus" aria-hidden="true"></i>Add New BL Form</a>
+                <h2>Table 1</h2>
               </tbody>
             </table>
           </div>  
         </div>  
       </div>  
-    @endcan
-    @can('daily-leads-list')
       <div class="tab-pane fade show" id="tab2">
         <div class="card-body">
           <div class="table-responsive">
@@ -53,18 +41,12 @@
                 </tr>
               </thead>
               <tbody>
-              <div hidden>{{$i=0;}}</div>
-                @foreach ($convertedleads as $key => $callsl)
-                  <tr data-id="1">
-                  <td>{{ ++$i }}</td>        
-                  </tr>
-                @endforeach
+                <h2>Table 2</h2>
               </tbody>
             </table>
           </div> 
         </div>  
-      </div> 
-      @endcan
+      </div>
       </div>
     </div>
   </div>
