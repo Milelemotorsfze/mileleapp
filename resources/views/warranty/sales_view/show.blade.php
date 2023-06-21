f9883576624f8f5ce6b653eb556d59532ddc0fd0
AnuMathewSunny <anumathew.sunny@milele.com>
2023-06-21 10:31:19 +0400

    [BUG FIX] - warranty policy details sales view page updated with design

--- resources/views/warranty/sales_view/show.blade.php
+++ resources/views/warranty/sales_view/show.blade.php
@@ -12,14 +12,13 @@
             <h4 class="card-title">Warranty Info</h4>
         </div>
         <div class="card-body">
-            <div class="row m-2">
+            <div class="row m-2" >
                 @foreach($warrantyBrands as $warrantyBrand)
-                <div class="col-lg-2 col-xxl-2 col-md-6 col-sm-12">
-                    <div class="card">
+                <div class="col-lg-2 col-xxl-2 col-md-6 col-sm-12" >
+                    <div class="card" style="min-height: 635px;">
                         <div class="card-body">
                             <div class="text-center bg-soft-secondary p-3 bg-row-{{$warrantyBrand->premium->PolicyName->id}}">
                                 <h4 class="text-black-50 " >{{$warrantyBrand->premium->PolicyName->name}}</h4>
-                                <i class="fa fa-wave-square"></i>
                                 <p>Claim Limit : {{ $warrantyBrand->premium->claim_limit_in_aed }} AED</p>
                                 <p class=" mb-2">Premium : @if($warrantyBrand->selling_price) {{ $warrantyBrand->selling_price }} AED @else Not Available @endif</p>
                                 <p>Extended Warranty Period : {{$warrantyBrand->premium->extended_warranty_period}} Months</p>
@@ -38,43 +37,46 @@
                                     @endif</p>
                             </div>
 
-                            <div class="bg-light p-3">
-                                <h6 class="text-center text-black-50"> BRANDS</h6>
-                                <div class="row justify-content-center border"  >
+                            <div class="p-3 pb-0">
+                                <h6 class="text-center "> BRANDS</h6>
+                                <div class="row justify-content-center"  >
                                     @foreach($warrantyBrand->policy_brands as $index => $brand)
-{{--                                        @if($index <= 2)--}}
-                                            <div class="list-group-flush" style="margin-left: 50px" ><i class="fa fa-check"> </i>  {{ $brand->brand_name }} </div>
-{{--                                        @else--}}
-{{--                                            @if($warrantyBrand->policy_brands->count() > 3)--}}
-{{--                                            <button class="btn bg-row-{{$warrantyBrand->premium->PolicyName->id}} btn-sm text-white w-25"--}}
-{{--                                                    data-bs-toggle="collapse" href="#collapseExample-{{$warrantyBrand->id}}"  aria-bs-controls="collapseExample"> More</button>--}}
-{{--                                                <div class="collapse" id="collapseExample-{{$warrantyBrand->id}}">--}}
-{{--                                                    <div class="card-body bg-light">--}}
-{{--                                                        <div class="list-group-flush"  ><i class="fa fa-check"> </i>--}}
-{{--                                                            {{ $brand->brand_name }} </div>--}}
-{{--                                                    </div>--}}
-{{--                                                </div>--}}
-{{--                                            @endif--}}
-{{--                                        @endif--}}
+                                        @if($index <= 2)
+                                            <div class="list-group-flush pb-2 show-less-{{$warrantyBrand->id}}"><i class="fa fa-check"> </i>&nbsp; &nbsp;  {{ $brand->brand_name }} </div>
+                                        @endif
                                     @endforeach
+                                    @if($warrantyBrand->policy_brands->count() > 3)
+                                            @foreach($warrantyBrand->policy_brands as $index => $brand)
+                                                <div class="collapse" id="collapseExample-{{$warrantyBrand->id}}">
+                                                        <div class="list-group-flush pb-1"  ><i class="fa fa-check"> </i>&nbsp; &nbsp;
+                                                            {{ $brand->brand_name }} </div>
+                                                </div>
+                                            @endforeach
+                                        <button class="btn btn-primary btn-sm text-white w-50 collapse-button mb-1" value="More" data-bs-toggle="collapse" href="#collapseExample-{{$warrantyBrand->id}}"
+                                                aria-bs-controls="collapseExample-{{$warrantyBrand->id}}" data-id="{{$warrantyBrand->id}}"> View More</button>
+
+                                    @endif
                                 </div>
-{{--                                <div class="row">--}}
-{{--                                @foreach($warrantyBrand->premium->PolicyName->warrantyPolicyCoverageParts as $key => $coveragePart)--}}
-{{--                                        @if($key <= 2)--}}
-{{--                                            <span class="list-group-flush" ><i class="fa fa-shield-alt"> </i>{{ $coveragePart->warrantyCoveragePart->name }}</span>--}}
-{{--                                        @else--}}
-{{--                                            @if($warrantyBrand->premium->PolicyName->warrantyPolicyCoverageParts->count() > 3)--}}
-{{--                                                <button class="btn bg-row-{{$warrantyBrand->premium->PolicyName->id}} btn-sm text-white w-25"--}}
-{{--                                                        data-bs-toggle="collapse" href="#collapseCoverage-{{$warrantyBrand->id}}"  aria-bs-controls="collapseExample"> More</button>--}}
-{{--                                                <div class="collapse" id="collapseCoverage-{{$warrantyBrand->id}}">--}}
-{{--                                                    <div class="card-body bg-light">--}}
-{{--                                                        <span class="list-group-flush" ><i class="fa fa-shield-alt"> </i>{{ $coveragePart->warrantyCoveragePart->name }}</span>--}}
-{{--                                                    </div>--}}
-{{--                                                </div>--}}
-{{--                                            @endif--}}
-{{--                                        @endif--}}
-{{--                                @endforeach--}}
-{{--                                </div>--}}
+                                <div class="row justify-content-center">
+                                    <h6 class="text-center mt-2"> COVERAGES</h6>
+                                    @foreach($warrantyBrand->premium->PolicyName->warrantyPolicyCoverageParts as $key => $coveragePart)
+                                        @if($key <= 2)
+                                            <span class="list-group-flush" ><i class="fa fa-check"> </i>&nbsp; &nbsp;{{ $coveragePart->warrantyCoveragePart->name }}</span>
+                                        @endif
+                                    @endforeach
+                                    @if($warrantyBrand->premium->PolicyName->warrantyPolicyCoverageParts->count() > 3)
+                                        @foreach($warrantyBrand->premium->PolicyName->warrantyPolicyCoverageParts as $key => $coveragePart)
+                                            <div class="collapse" id="collapseCoverage-{{$warrantyBrand->id}}">
+                                                <div class="view-more-{{$warrantyBrand->id}}">
+                                                    <span class="list-group-flush" ><i class="fa fa-check"> </i>&nbsp; &nbsp;{{ $coveragePart->warrantyCoveragePart->name }}</span>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                        <button class="btn btn-primary btn-sm text-white w-50 view-more-button mt-2"
+                                                data-bs-toggle="collapse" href="#collapseCoverage-{{$warrantyBrand->id}}" data-id="{{$warrantyBrand->id}}"
+                                                aria-bs-controls="collapseCoverage">View More</button>
+                                    @endif
+                                </div>
                             </div>
 
                         </div>
@@ -85,3 +87,33 @@
         </div>
 @endcan
 @endsection
+@push('scripts')
+    <script>
+        $(".collapse-button").click(function(){
+            var $this = $(this);
+            var id = $(this).attr('data-id');
+
+            $this.toggleClass('collapse-button');
+            if($this.hasClass('collapse-button')){
+                $this.text('View More');
+                $('.show-less-'+id).show();
+            } else {
+                $this.text('View Less');
+                $('.show-less-'+id).hide();
+            }
+        });
+        $(".view-more-button").click(function(){
+            var $this = $(this);
+            var id = $(this).attr('data-id');
+
+            $this.toggleClass('collapse-button');
+            if($this.hasClass('collapse-button')){
+                $this.text('View More');
+                $('.view-more-'+id).show();
+            } else {
+                $this.text('View Less');
+                $('.view-more-'+id).hide();
+            }
+        });
+    </script>
+@endpush
