105bad64835bcc7b659862bf93d6f980e1689883
it@milele.com <it@milele.com>
2023-06-26 14:56:59 +0400

    Comit after server uploading

---
+++ database/migrations/2023_06_26_071030_update_vehicles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropForeign('vehicles_conversion_id_foreign');
+            $table->dropColumn('conversion_id'); 
+            $table->string('conversion'); 
+        });
+    }
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('conversion'); 
+            $table->foreign('conversion_id')
+                ->references('id')
+                ->on('conversions')
+                ->onDelete('cascade');
+        });
+    }
+};
