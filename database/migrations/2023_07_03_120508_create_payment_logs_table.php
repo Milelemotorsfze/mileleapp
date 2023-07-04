a78560fb0d3115c1fbccf0922d2402172b750ace
it@milele.com <it@milele.com>
2023-07-04 09:05:31 +0400

    Stock Report Changes 04-07-2023

---
+++ database/migrations/2023_07_03_120508_create_payment_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_logs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('created_by')->unsigned()->index()->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->bigInteger('vehicle_id')->unsigned()->index()->nullable();
+            $table->foreign('vehicle_id')->references('id')->on('vehicles');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_logs');
+    }
+};
