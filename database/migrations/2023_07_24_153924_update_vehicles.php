2884a64ecfa4b458c045d2e41ff78099ad9a7cef
waqar1944 <waqar.younas@milele.com>
2023-07-24 17:51:54 +0400

    Changes uploading to the server

---
+++ database/migrations/2023_07_24_153924_update_vehicles.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->bigInteger('latest_location')->unsigned()->index()->nullable();
+            $table->foreign('latest_location')->references('id')->on('warehouse')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            //
+        });
+    }
+};
