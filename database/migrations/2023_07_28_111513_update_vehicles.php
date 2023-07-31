0fc77909746981aaf608870529cdf9f9d1c41f92
waqar1944 <waqar.younas@milele.com>
2023-07-31 11:17:06 +0400

    Update Stock report pagination

---
+++ database/migrations/2023_07_28_111513_update_vehicles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->string('extra_features')->nullable();
+            $table->string('qc_remarks')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('extra_features');
+            $table->dropColumn('qc_remarks');
+        });
+    }
+};
