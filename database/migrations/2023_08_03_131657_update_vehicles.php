91fc01fe33df8d436dec263173af6f96576fe8e2
waqar1944 <waqar.younas@milele.com>
2023-08-03 14:29:54 +0400

    Adding netsuit date and number

---
+++ database/migrations/2023_08_03_131657_update_vehicles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->text('netsuit_grn_number')->nullable();
+            $table->date('netsuit_grn_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('netsuit_grn_number');
+            $table->dropColumn('netsuit_grn_date');
+        });
+    }
+};
