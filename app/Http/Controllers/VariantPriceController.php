e69224eaf59ed2c4e3abd061c9736363b49a2447
AnuMathewSunny <anumathew.sunny@milele.com>
2023-08-30 16:05:14 +0400

    [FEAT] - vehicle table attribute changed to controller

--- app/Http/Controllers/VariantPriceController.php
+++ app/Http/Controllers/VariantPriceController.php
@@ -7,6 +7,7 @@ use App\Models\AvailableColour;
 use App\Models\Varaint;
 use App\Models\VehiclePriceHistory;
 use App\Models\Vehicles;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,13 +23,85 @@ class VariantPriceController extends Controller
                                 ->where('payment_status', $statuss)
                                 ->groupBy('varaints_id')
                                 ->selectRaw('count(*) as total,id, varaints_id, int_colour, ex_colour, price')->get();
-        $activeStocks = $activeStocks->sortBy('price_status');
+
         $InactiveStocks =  Vehicles::whereNotNull('gdn_id')
                                 ->where('payment_status', $statuss)
                                 ->groupBy('varaints_id')
                                 ->selectRaw('count(*) as total,id, varaints_id, int_colour, ex_colour, price')
                                 ->get();
+        $activeStocks = $activeStocks->sortBy('price_status');
         $InactiveStocks = $InactiveStocks->sortBy('price_status');
+        foreach ($activeStocks as $activeStock) {
+
+            $activeStock->similar_vehicles_with_active_stock =  Vehicles::whereNull('gdn_id')->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+                                                                    ->where('varaints_id', $activeStock->varaints_id)
+                                                                    ->groupBy('int_colour','ex_colour')
+                                                                    ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+                                                                    ->get();
+
+            $similarVehicles = Vehicles::whereNull('gdn_id')
+                ->where('varaints_id', $activeStock->varaints_id)
+                ->where('int_colour', $activeStock->int_colour)
+                ->where('ex_colour', $activeStock->ex_colour)
+                ->pluck('id');
+
+            $priceStatus = [];
+
+            foreach ($similarVehicles as $similarVehicle) {
+                $vehicle = Vehicles::find($similarVehicle);
+                $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+                    ->where('int_colour', $vehicle->int_colour)
+                    ->where('ext_colour', $vehicle->ex_colour)
+                    ->first();
+                if(!empty($availableColour)) {
+                    $priceStatus[] = 'true';
+                }else{
+                    $priceStatus[] = 'false';
+                }
+            }
+            if (array_unique($priceStatus) === array('true')) {
+                $activeStock->active_vehicle_price_status = 1;
+            }else{
+                $activeStock->active_vehicle_price_status = 0;
+            }
+        }
+        foreach ($InactiveStocks as $InactiveStock) {
+
+                $InactiveStock->similar_vehicles_with_inactive_stock = Vehicles::whereNotNull('gdn_id')
+                    ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+                    ->where('varaints_id', $InactiveStock->varaints_id)
+                    ->groupBy('int_colour', 'ex_colour')
+                    ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+                    ->get();
+
+            $similarVehicles = Vehicles::whereNotNull('gdn_id')
+                ->where('varaints_id', $InactiveStock->varaints_id)
+                ->where('int_colour', $InactiveStock->int_colour)
+                ->where('ex_colour', $InactiveStock->ex_colour)
+                ->pluck('id');
+
+            $priceStatus = [];
+
+            foreach ($similarVehicles as $similarVehicle) {
+                $vehicle = Vehicles::find($similarVehicle);
+                $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+                    ->where('int_colour', $vehicle->int_colour)
+                    ->where('ext_colour', $vehicle->ex_colour)
+                    ->first();
+                if(!empty($availableColour)) {
+                    $priceStatus[] = 'true';
+                }else{
+                    $priceStatus[] = 'false';
+                }
+            }
+            if (array_unique($priceStatus) === array('true')) {
+                $InactiveStock->inactive_vehicle_price_status = 1;
+            }else{
+                $InactiveStock->inactive_vehicle_price_status = 0;
+            }
+        }
+
+//        dd($activeStocks);
 
         return view('variant-prices.index', compact('activeStocks','InactiveStocks'));
     }
@@ -67,11 +140,78 @@ class VariantPriceController extends Controller
         $VariantPrices = AvailableColour::where('varaint_id', $vehicle->varaints_id)
             ->pluck('id');
         if($type == 1) {
-            $vehicles =  $vehicle->similar_vehicles_with_active_stock;
+         $similarVehicles =  Vehicles::whereNull('gdn_id')
+                ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+                ->where('varaints_id', $vehicle->varaints_id)
+                ->groupBy('int_colour','ex_colour')
+                ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+                ->get();
+
+            $vehicles =  $similarVehicles->sortBy('price_status');
+
+            foreach ($vehicles as $eachVehicle) {
+                $availableColour = AvailableColour::where('varaint_id', $eachVehicle->varaints_id)
+                    ->where('int_colour', $eachVehicle->int_colour)
+                    ->where('ext_colour', $eachVehicle->ex_colour)
+                    ->first();
+
+                $eachVehicle->old_price = 0;
+                $eachVehicle->old_price_dated = "";
+                $eachVehicle->updated_by = "";
+                if(!empty($availableColour)) {
+                    $eachVehicle->updated_by = $availableColour->user->name;
+                    $priceHistories = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
+                        ->orderBy('id','DESC');
+                    if($priceHistories->count() > 1 ) {
+                        $latestPrice = $priceHistories->skip(1)->first();
+                        $eachVehicle->old_price_dated = Carbon::parse($latestPrice->updated_at)->format('d/m/y, H:i:s');
+                    }
+                    $latestPriceHistory = $priceHistories->first();
+                    if(!empty($latestPriceHistory)) {
+                        $eachVehicle->old_price  =  $latestPriceHistory->old_price;
+                    }
+                }
+
+            }
             $variantPriceHistories = VehiclePriceHistory::whereIn('available_colour_id', $VariantPrices)
                 ->orderBy('id','DESC')->get();
         }else{
-            $vehicles =  $vehicle->similar_vehicles_with_inactive_stock;
+            $similarVehicles = Vehicles::whereNotNull('gdn_id')
+                ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+                ->where('varaints_id', $vehicle->varaints_id)
+                ->groupBy('int_colour', 'ex_colour')
+                ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+                ->get();
+
+            $vehicles =  $similarVehicles->sortBy('price_status');
+
+            foreach ($vehicles as $eachVehicle) {
+                $availableColour = AvailableColour::where('varaint_id', $eachVehicle->varaints_id)
+                    ->where('int_colour', $eachVehicle->int_colour)
+                    ->where('ext_colour', $eachVehicle->ex_colour)
+                    ->first();
+
+                $eachVehicle->old_price = 0;
+                $eachVehicle->old_price_dated = " ";
+                $eachVehicle->updated_by = " ";
+                if(!empty($availableColour)) {
+                    $eachVehicle->updated_by = $availableColour->user->name;
+                    $priceHistories = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
+                        ->orderBy('id','DESC');
+
+                    if($priceHistories->count() > 1 ) {
+                        $latestPrice = $priceHistories->skip(1)->first();
+                        $eachVehicle->old_price_dated = Carbon::parse($latestPrice->updated_at)->format('d/m/y, H:i:s');
+                    }
+                    $latestPriceHistory = $priceHistories->first();
+                    if(!empty($latestPriceHistory)) {
+                        $eachVehicle->old_price  =  $latestPriceHistory->old_price;
+                    }
+                }
+
+
+            }
+
             $variantPriceHistories = VehiclePriceHistory::whereIn('available_colour_id', $VariantPrices)
                 ->orderBy('id','DESC')->get();
        }
--- app/Models/Vehicles.php
+++ app/Models/Vehicles.php
@@ -11,7 +11,6 @@ class Vehicles extends Model
     use HasFactory;
     protected $table = 'vehicles';
     public const VEHICLE_STATUS_INCOMING = 'Incoming Stock';
-
     public const FILTER_PREVIOUS_YEAR_SOLD = 'PREVIOUS YEAR SOLD';
     public const FILTER_PREVIOUS_MONTH_SOLD = 'PREVIOUS MONTH SOLD';
     public const FILTER_YESTERDAY_SOLD = 'YESTERDAY SOLD';
@@ -27,13 +26,13 @@ class Vehicles extends Model
 
 
     public  $appends = [
-        'similar_vehicles_with_active_stock',
-        'similar_vehicles_with_inactive_stock',
-        'old_price',
-        'old_price_dated',
-        'updated_by',
-        'active_vehicle_price_status',
-        'inactive_vehicle_price_status'
+//        'similar_vehicles_with_active_stock',
+//        'similar_vehicles_with_inactive_stock',
+//        'old_price',
+//        'old_price_dated',
+//        'updated_by',
+//        'active_vehicle_price_status',
+//        'inactive_vehicle_price_status'
 
     ];
     protected $fillable = [
@@ -76,149 +75,136 @@ class Vehicles extends Model
     {
         return $this->belongsTo(Gdn::class, 'gdn_id');
     }
-    public function getSimilarVehiclesWithInactiveStockAttribute()
-    {
-        $vehicles = Vehicles::whereNotNull('gdn_id')
-            ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
-            ->where('varaints_id', $this->varaints_id)
-            ->groupBy('int_colour', 'ex_colour')
-            ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
-            ->get();
-        $vehicles = $vehicles->sortBy('price_status');
+//    public function getSimilarVehiclesWithInactiveStockAttribute()
+//    {
+//        $vehicles = Vehicles::whereNotNull('gdn_id')
+//            ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+//            ->where('varaints_id', $this->varaints_id)
+//            ->groupBy('int_colour', 'ex_colour')
+//            ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+//            ->get();
+//        $vehicles = $vehicles->sortBy('price_status');
+//
+//        return $vehicles;
+//    }
+//    public function getSimilarVehiclesWithActiveStockAttribute() {
+//        $vehicles =  Vehicles::whereNull('gdn_id')
+//            ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
+//            ->where('varaints_id', $this->varaints_id)
+//            ->groupBy('int_colour','ex_colour')
+//            ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
+//            ->get();
+//        $vehicles = $vehicles->sortBy('price_status');
+//
+//        return $vehicles;
+//    }
 
-        return $vehicles;
-    }
-    public function getSimilarVehiclesWithActiveStockAttribute() {
-        $vehicles =  Vehicles::whereNull('gdn_id')
-            ->where('payment_status', Vehicles::VEHICLE_STATUS_INCOMING)
-            ->where('varaints_id', $this->varaints_id)
-            ->groupBy('int_colour','ex_colour')
-            ->selectRaw('count(*) as count,id, varaints_id, int_colour, ex_colour, price')
-            ->get();
-        $vehicles = $vehicles->sortBy('price_status');
+//    public function getOldPriceAttribute() {
+//
+//        $vehicle = Vehicles::find($this->id);
+//        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+//            ->where('int_colour', $vehicle->int_colour)
+//            ->where('ext_colour', $vehicle->ex_colour)
+//            ->first();
+//        if(!empty($availableColour)) {
+//            $latestPriceHistory = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
+//                ->orderBy('id','DESC')
+//                ->first();
+//            if(!empty($availableColour)) {
+//                return $latestPriceHistory->old_price;
+//            }
+//            return 0;
+//        }
+//        return 0;
+//    }
+//    public function getActiveVehiclePriceStatusAttribute() {
+//
+//        $similarVehicles = Vehicles::whereNull('gdn_id')
+//            ->where('varaints_id', $this->varaints_id)
+//            ->where('int_colour', $this->int_colour)
+//            ->where('ex_colour', $this->ex_colour)
+//            ->pluck('id');
+//
+//        $priceStatus = [];
+//
+//        foreach ($similarVehicles as $similarVehicle) {
+//            $vehicle = Vehicles::find($similarVehicle);
+//            $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+//                ->where('int_colour', $vehicle->int_colour)
+//                ->where('ext_colour', $vehicle->ex_colour)
+//                ->first();
+//            if(!empty($availableColour)) {
+//                $priceStatus[] = 'true';
+//            }else{
+//                $priceStatus[] = 'false';
+//            }
+//        }
+//        if (array_unique($priceStatus) === array('true')) {
+//            return 1;
+//        }else{
+//            return 0;
+//        }
+//    }
+//    public function getInactiveVehiclePriceStatusAttribute() {
+//
+//        $similarVehicles = Vehicles::whereNotNull('gdn_id')
+//            ->where('varaints_id', $this->varaints_id)
+//            ->where('int_colour', $this->int_colour)
+//            ->where('ex_colour', $this->ex_colour)
+//            ->pluck('id');
+//
+//        $priceStatus = [];
+//
+//        foreach ($similarVehicles as $similarVehicle) {
+//            $vehicle = Vehicles::find($similarVehicle);
+//            $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+//                ->where('int_colour', $vehicle->int_colour)
+//                ->where('ext_colour', $vehicle->ex_colour)
+//                ->first();
+//            if(!empty($availableColour)) {
+//                $priceStatus[] = 'true';
+//            }else{
+//                $priceStatus[] = 'false';
+//            }
+//        }
+//        if (array_unique($priceStatus) === array('true')) {
+//            return 1;
+//        }else{
+//            return 0;
+//        }
+//    }
+//    public function getOldPriceDatedAttribute() {
+//
+//        $vehicle = Vehicles::find($this->id);
+//        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+//            ->where('int_colour', $vehicle->int_colour)
+//            ->where('ext_colour', $vehicle->ex_colour)
+//            ->first();
+//        if(!empty($availableColour)) {
+//            $latestPriceHistory = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
+//                ->orderBy('id', 'DESC')
+//                ->get();
+//            if ($latestPriceHistory->count() > 1 ) {
+//                $latestPriceHistory = $latestPriceHistory->skip(1)->first();
+//                return Carbon::parse($latestPriceHistory->updated_at)->format('d/m/y, H:i:s');
+//            }
+//
+//        }
+//            return "";
+//    }
+//    public function getUpdatedByAttribute() {
+//
+//        $vehicle = Vehicles::find($this->id);
+//        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
+//            ->where('int_colour', $vehicle->int_colour)
+//            ->where('ext_colour', $vehicle->ex_colour)
+//            ->first();
+//        if(!empty($availableColour)) {
+//            $updatedBy = $availableColour->user->name;
+//           return $updatedBy;
+//        }
+//        return " ";
+//    }
 
-        return $vehicles;
-    }
-
-    public function getOldPriceAttribute() {
-
-        $vehicle = Vehicles::find($this->id);
-        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-            ->where('int_colour', $vehicle->int_colour)
-            ->where('ext_colour', $vehicle->ex_colour)
-            ->first();
-        if(!empty($availableColour)) {
-            $latestPriceHistory = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
-                ->orderBy('id','DESC')
-                ->first();
-            if(!empty($availableColour)) {
-                return $latestPriceHistory->old_price;
-            }
-            return 0;
-        }
-        return 0;
-    }
-    public function getActiveVehiclePriceStatusAttribute() {
-
-        $similarVehicles = Vehicles::whereNull('gdn_id')
-            ->where('varaints_id', $this->varaints_id)
-            ->where('int_colour', $this->int_colour)
-            ->where('ex_colour', $this->ex_colour)
-            ->pluck('id');
-
-        $priceStatus = [];
-
-        foreach ($similarVehicles as $similarVehicle) {
-            $vehicle = Vehicles::find($similarVehicle);
-            $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-                ->where('int_colour', $vehicle->int_colour)
-                ->where('ext_colour', $vehicle->ex_colour)
-                ->first();
-            if(!empty($availableColour)) {
-                $priceStatus[] = 'true';
-            }else{
-                $priceStatus[] = 'false';
-            }
-        }
-        if (array_unique($priceStatus) === array('true')) {
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-    public function getInactiveVehiclePriceStatusAttribute() {
-
-        $similarVehicles = Vehicles::whereNotNull('gdn_id')
-            ->where('varaints_id', $this->varaints_id)
-            ->where('int_colour', $this->int_colour)
-            ->where('ex_colour', $this->ex_colour)
-            ->pluck('id');
-
-        $priceStatus = [];
-
-        foreach ($similarVehicles as $similarVehicle) {
-            $vehicle = Vehicles::find($similarVehicle);
-            $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-                ->where('int_colour', $vehicle->int_colour)
-                ->where('ext_colour', $vehicle->ex_colour)
-                ->first();
-            if(!empty($availableColour)) {
-                $priceStatus[] = 'true';
-            }else{
-                $priceStatus[] = 'false';
-            }
-        }
-        if (array_unique($priceStatus) === array('true')) {
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-    public function getOldPriceDatedAttribute() {
-
-        $vehicle = Vehicles::find($this->id);
-        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-            ->where('int_colour', $vehicle->int_colour)
-            ->where('ext_colour', $vehicle->ex_colour)
-            ->first();
-        if(!empty($availableColour)) {
-            $latestPriceHistory = VehiclePriceHistory::where('available_colour_id', $availableColour->id)
-                ->orderBy('id', 'DESC')
-                ->get();
-            if ($latestPriceHistory->count() > 1 ) {
-                $latestPriceHistory = $latestPriceHistory->skip(1)->first();
-                return Carbon::parse($latestPriceHistory->updated_at)->format('d/m/y, H:i:s');
-            }
-
-        }
-            return "";
-    }
-    public function getUpdatedByAttribute() {
-
-        $vehicle = Vehicles::find($this->id);
-        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-            ->where('int_colour', $vehicle->int_colour)
-            ->where('ext_colour', $vehicle->ex_colour)
-            ->first();
-        if(!empty($availableColour)) {
-            $updatedBy = $availableColour->user->name;
-           return $updatedBy;
-        }
-        return " ";
-    }
-    public function getUpdatedAtAttribute() {
-
-        $vehicle = Vehicles::find($this->id);
-        $availableColour = AvailableColour::where('varaint_id', $vehicle->varaints_id)
-            ->where('int_colour', $vehicle->int_colour)
-            ->where('ext_colour', $vehicle->ex_colour)
-            ->first();
-        if(!empty($availableColour)) {
-            $updatedAt = $availableColour->updated_at;
-            return Carbon::parse($updatedAt)->format('d/m/Y, H:i:s');
-            return  ;
-        }
-        return " ";
-    }
 
 }
