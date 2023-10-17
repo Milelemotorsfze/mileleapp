57a7db279d52e083d1f95943a37d6751e9659ae7
waqar1944 <waqar.younas@milele.com>
2023-10-17 10:00:24 +0400

    Change into inspection module

---
+++ app/Models/UserActivities.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserActivities extends Model
+{
+    use HasFactory;
+    protected $table = 'user_activities';
+    protected $fillable = [
+        'activity',
+        'users_id',
+        'created_at'
+    ];
+}
