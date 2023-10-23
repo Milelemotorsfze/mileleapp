83ef2dcc6ae9b97d95e22521c05ebd4c8c176d6c
Rejitha Rajendran <rejitha.rajendran@milele.com>
2023-10-23 16:27:08 +0400

    user activity implemented in MasterAddonController

--- app/Http/Controllers/LoginActivityController.php
+++ app/Http/Controllers/LoginActivityController.php
@@ -8,21 +8,13 @@ use Carbon\Carbon;
 
 class LoginActivityController extends Controller
 {
-    public function listUsers()
-    {
+    public function listUsers() {
         return view('users.activity.loginactivity');
     }
-public function listUsersgetdata(Request $request) {
-    $startDate = Carbon::createFromFormat('Y-m-d', $request->input('start_date'))->startOfDay();
-    $endDate = Carbon::createFromFormat('Y-m-d', $request->input('end_date'))->endOfDay();
-
-    info($startDate);
-    info($endDate);
-
-    $data = LogActivity::with('logineUser')
-        ->whereBetween('created_at', [$startDate, $endDate])
-        ->get();
-
-    return response()->json(['data' => $data]);
-}
+    public function listUsersgetdata(Request $request) {
+        $startDate = Carbon::createFromFormat('Y-m-d', $request->input('start_date'))->startOfDay();
+        $endDate = Carbon::createFromFormat('Y-m-d', $request->input('end_date'))->endOfDay();
+        $data = LogActivity::with('logineUser')->whereBetween('created_at', [$startDate, $endDate])->get();
+        return response()->json(['data' => $data]);
+    }
 }
