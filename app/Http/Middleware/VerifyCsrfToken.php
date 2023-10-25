7ae66d560a1d646e74d43ce8fa7df283196c403b
AnuMathewSunny <anumathew.sunny@milele.com>
2023-10-25 17:06:33 +0400

    [FEAT] - pfi unique validation added

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-//        'pfi/reference-number-unique-check'
+        '/reference-number-unique-check'
     ];
 }
