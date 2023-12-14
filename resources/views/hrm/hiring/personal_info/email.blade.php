d535dff8ace45391d1d868d8fe764e3334e01039
Rejitha Rajendran <rejitha.rajendran@milele.com>
2023-12-14 13:03:51 +0400

    approvals permission and view supplier details in view addon page

---
+++ resources/views/hrm/hiring/personal_info/email.blade.php
@@ -0,0 +1,6 @@
+<h4>{{ $data["name"] }}</h4>
+<p>We are very glad to inform you that, you are shortlisted for this job position. Kindly fill the attached form for further procedures. 
+link is - <a href="{{env('BASE_URL')}}/candidate/personal_info/{{ $data["id"] }}">Candidate Personal Information Form</a>
+</p>
+<p>Regards,</p>
+<p>Milele Matrix</p>
