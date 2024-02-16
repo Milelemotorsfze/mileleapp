1179b91563a726f1ec3f011a54db47a6682db482
Rejitha Rajendran <rejitha.rajendran@milele.com>
2024-02-16 10:17:26 +0400

    HRMS mosule and permission seeder

--- database/seeders/HRMSPermissionSeeder.php
+++ database/seeders/HRMSPermissionSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
+use App\Models\Modules;
+use App\Models\Permission;
 
 class HRMSPermissionSeeder extends Seeder
 {
