5a09c676e17feec78d98a7437d20c48bc61cf996
waqar1944 <waqar.younas@milele.com>
2024-02-28 14:39:35 +0400

    Update Qoutation Brand ID & Preorder

--- app/Http/Controllers/PreOrderController.php
+++ app/Http/Controllers/PreOrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\PreOrder;
+use App\Models\UserActivities;
 use App\Models\Brand;
 use App\Models\ColorCode;
 use App\Models\Quotation;
@@ -19,9 +20,59 @@ class PreOrderController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $useractivities =  New UserActivities();
+        $useractivities->activity = "Open Pre Order View Purchasing";
+        $useractivities->users_id = Auth::id();
+        $useractivities->save();
+        if ($request->ajax()) {
+            $status = $request->input('status');
+            $searchValue = $request->input('search.value');
+            if($status === "Pending")
+            {
+            $data = PreOrder::select( [
+                    'vehicles.id',
+                    'warehouse.name as location',
+                     DB::raw("DATE_FORMAT(purchasing_order.po_date, '%d-%b-%Y') as po_date"),
+                    'vehicles.ppmmyyy',
+                    'vehicles.vin',
+                    'brands.brand_name',
+                    'varaints.name as variant',
+                    'varaints.model_detail',
+                    'varaints.detail',
+                    'varaints.seat',
+                    'varaints.upholestry',
+                    'varaints.steering',
+                    'varaints.my',
+                    'varaints.fuel_type',
+                    'varaints.gearbox',
+                    'master_model_lines.model_line',
+                    'int_color.name as interior_color',
+                    'ex_color.name as exterior_color',
+                    'purchasing_order.po_number',
+                    'grn.grn_number',
+                    DB::raw("DATE_FORMAT(grn.date, '%d-%b-%Y') as date"),
+                ])
+                ->leftJoin('purchasing_order', 'vehicles.purchasing_order_id', '=', 'purchasing_order.id')
+                ->leftJoin('warehouse', 'vehicles.latest_location', '=', 'warehouse.id')
+                ->leftJoin('grn', 'vehicles.grn_id', '=', 'grn.id')
+                ->leftJoin('color_codes as int_color', 'vehicles.int_colour', '=', 'int_color.id')
+                ->leftJoin('color_codes as ex_color', 'vehicles.ex_colour', '=', 'ex_color.id')
+                ->leftJoin('varaints', 'vehicles.varaints_id', '=', 'varaints.id')
+                ->leftJoin('master_model_lines', 'varaints.master_model_lines_id', '=', 'master_model_lines.id')
+                ->leftJoin('brands', 'varaints.brands_id', '=', 'brands.id')
+                ->leftJoin('inspection', 'vehicles.id', '=', 'inspection.vehicle_id')
+                ->whereNull('inspection.id')
+                ->whereNull('vehicles.inspection_date')
+                ->whereNull('vehicles.gdn_id')
+                ->whereNotNull('vehicles.grn_id');
+                $data = $data->groupBy('vehicles.id');
+            }
+                return DataTables::of($data)
+                ->toJson();
+        }
+        return view('preorder.index');
     }
 
     /**
