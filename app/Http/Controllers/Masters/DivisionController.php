8f32e3b964f778b24d54fe606878876bfa4bb54b
Rejitha Rajendran <rejitha.rajendran@milele.com>
2024-04-19 14:22:26 +0400

    fixed error due to new implementation as management dept without lead

--- app/Http/Controllers/Masters/DivisionController.php
+++ app/Http/Controllers/Masters/DivisionController.php
@@ -28,7 +28,7 @@ class DivisionController extends Controller
         $data = MasterDivisionWithHead::where('id',$id)->first();
         $previous = MasterDivisionWithHead::where('id', '<', $id)->max('id');
         $next = MasterDivisionWithHead::where('id', '>', $id)->min('id');
-        $divisionHeads = User::where('status','active')->whereNotIn('id',[1,16])->whereHas('empProfile')->with('empProfile.department','empProfile.designation','empProfile.location')->whereIn('id',[2,26,31,10,62,57])->get();
+        $divisionHeads = User::where('status','active')->whereNotIn('id',[1,16])->whereHas('empProfile')->with('empProfile.designation','empProfile.location')->whereIn('id',[2,26,31,78])->get();
         return view('hrm.masters.division.edit',compact('data','previous','next','divisionHeads'));
     }
     public function update(Request $request, $id) {
