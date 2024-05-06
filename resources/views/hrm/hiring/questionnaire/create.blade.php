a94513b3a7ba9bfd4067a718377750b7e7d9bc44
Rejitha Rajendran <rejitha.rajendran@milele.com>
2024-05-06 09:57:59 +0400

    restrict questionnaire creation from multiple users at the same time for same hiring request

--- resources/views/hrm/hiring/questionnaire/create.blade.php
+++ resources/views/hrm/hiring/questionnaire/create.blade.php
@@ -77,6 +77,18 @@ $hasPermission = Auth::user()->hasPermissionForSelectedRole('create-questionnair
 		</ul>
 	</div>
 	@endif
+	@if (Session::has('error'))
+	<div class="alert alert-danger" >
+		<button type="button" class="btn-close p-0 close" data-dismiss="alert">x</button>
+		{{ Session::get('error') }}
+	</div>
+	@endif
+	@if (Session::has('success'))
+	<div class="alert alert-success" id="success-alert">
+		<button type="button" class="btn-close p-0 close" data-dismiss="alert">x</button>
+		{{ Session::get('success') }}
+	</div>
+	@endif
 	<!-- {!! Form::open(array('route' => 'calls.store','method'=>'POST', 'id' => 'calls')) !!} -->
 	@include('hrm.hiring.hiring_request.details')
 	<div class="row">
@@ -84,6 +96,7 @@ $hasPermission = Auth::user()->hasPermissionForSelectedRole('create-questionnair
 	</div>
 	<form id="employeeQuestionnaireForm" name="employeeQuestionnaireForm" enctype="multipart/form-data" method="POST" action="{{route('employee-hiring-questionnaire.store-or-update',$data->id)}}">
 		@csrf
+		<input type="hidden" name="questionnaire_id" value="{{$currentQuestionnaire->id ?? ''}}">
 		<div class="row">
 			<div class=" col-lg-4 col-md-6 col-sm-6 radio-button-main-div">
 				<div class="row">
