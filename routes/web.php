41fb2b49d998f2add4d54dcb201856c06bcf49ea
waqar1944 <waqar.younas@milele.com>
2024-06-07 17:10:21 +0400

    Vehicle Current status

--- routes/web.php
+++ routes/web.php
@@ -586,6 +586,8 @@ Route::get('/d', function () {
     Route::resource('Vehicles', VehiclesController::class);
     Route::get('vehicles/filter', [VehiclesController::class, 'index'])->name('vehicles.filter');
     Route::get('vehicles/statuswise', [VehiclesController::class, 'statuswise'])->name('vehicles.statuswise');
+    Route::get('vehicles/currentstatus', [VehiclesController::class, 'currentstatus'])->name('vehicles.currentstatus');
+    Route::post('vehicles/statussreach', [VehiclesController::class, 'statussreach'])->name('vehicles.statussearch');
     // Route::get('/search-data', [VehiclesController::class, 'searchData'])->name('vehicles.search-data');
     Route::post('purchasing-order/check-po-number', [PurchasingOrderController::class, 'checkPONumber'])->name('purchasing-order.checkPONumber');
     Route::post('purchasing-order/updatebasicdetails', [PurchasingOrderController::class, 'updatebasicdetails'])->name('purchasing-order.updatebasicdetails');
