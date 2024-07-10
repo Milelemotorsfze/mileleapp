51b5f4d193542343d9e26e92dc7dd94d17661cbd
waqar1944 <waqar.younas@milele.com>
2024-07-10 10:50:10 +0400

    fixing the issue of drop down in purchased order

--- resources/views/layouts/datatable.blade.php
+++ resources/views/layouts/datatable.blade.php
@@ -4,8 +4,6 @@
 @include('partials/head-css')
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-
 </head>
 <body data-layout="horizontal">
     <div id="layout-wrapper">
