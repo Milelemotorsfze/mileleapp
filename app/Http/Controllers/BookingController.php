b231332658e8f065eff26fe31ffc23a892691c18
waqar1944 <waqar.younas@milele.com>
2024-08-22 12:33:56 +0400

    Booking without so issue solved

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -324,9 +324,6 @@ public function store(Request $request)
             ->where('booking_requests.status', $status)
             ->whereDate('booking.booking_end_date', '>=', now())
             ->whereNotNull('vehicles.so_id');
-            if ($hasEditSOPermission) {
-                $data = $data->where('booking_requests.created_by', Auth::id());
-            }
             if (!empty($searchValue)) {
                 $data->where(function ($query) use ($searchValue) {
                     $query->where('booking_requests.days', 'like', '%' . $searchValue . '%')
@@ -381,9 +378,6 @@ public function store(Request $request)
             ->leftJoin('so', 'vehicles.so_id', '=', 'so.id')
             ->where('booking_requests.status', $status)
             ->whereDate('booking.booking_end_date', '<', now());
-            if ($hasEditSOPermission) {
-                $data = $data->where('booking_requests.created_by', Auth::id());
-            }
             if (!empty($searchValue)) {
                 $data->where(function ($query) use ($searchValue) {
                     $query->where('booking_requests.days', 'like', '%' . $searchValue . '%')
@@ -437,9 +431,6 @@ public function store(Request $request)
             ->leftJoin('users', 'booking_requests.created_by', '=', 'users.id')
             ->leftJoin('so', 'vehicles.so_id', '=', 'so.id')
             ->where('booking_requests.status', $status);
-            if ($hasEditSOPermission) {
-                $data = $data->where('booking_requests.created_by', Auth::id());
-            }
             if (!empty($searchValue)) {
                 $data->where(function ($query) use ($searchValue) {
                     $query->where('booking_requests.days', 'like', '%' . $searchValue . '%')
