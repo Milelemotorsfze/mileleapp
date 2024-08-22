256c73e46c8d509d247bf99a9e255dafed49b6ca
waqar1944 <waqar.younas@milele.com>
2024-08-22 09:48:28 +0400

    Update stock price

--- resources/views/vehicles/stock.blade.php
+++ resources/views/vehicles/stock.blade.php
@@ -1020,23 +1020,23 @@
         { data: 'fd', name: 'countries.name' },
     ];
     if (hasPricePermission) {
-        // columns3.push({
-        //     data: 'price', 
-        //     name: 'vehicles.price', 
-        //             render: function(data, type, row) {
-        //                 if (data) {
-        //                     // Convert the string to a float, then format it with commas
-        //                     var formattedPrice = parseFloat(data).toLocaleString('en-US', {
-        //                         minimumFractionDigits: 0,
-        //                         maximumFractionDigits: 0
-        //                     });
+        columns3.push({
+            data: 'price', 
+            name: 'vehicles.price', 
+                    render: function(data, type, row) {
+                        if (data) {
+                            // Convert the string to a float, then format it with commas
+                            var formattedPrice = parseFloat(data).toLocaleString('en-US', {
+                                minimumFractionDigits: 0,
+                                maximumFractionDigits: 0
+                            });
 
-        //                     // Return the price wrapped in a span with button-like styling
-        //                     return '<span style="display: inline-block; background-color: #28a745; color: white; padding: 5px 10px; border-radius: 5px; font-weight: bold;">' + formattedPrice + '</span>';
-        //                 }
-        //                 return ''; // Return an empty string if there's no price
-        //             }
-        // });
+                            // Return the price wrapped in a span with button-like styling
+                            return '<span style="display: inline-block; background-color: #28a745; color: white; padding: 5px 10px; border-radius: 5px; font-weight: bold;">' + formattedPrice + '</span>';
+                        }
+                        return ''; // Return an empty string if there's no price
+                    }
+        });
     }
     columns3.push(
         { data: 'po_number', name: 'purchasing_order.po_number' },
