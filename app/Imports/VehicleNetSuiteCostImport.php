0c6598089a10978e69530459f82d87ea9e3db493
waqar1944 <waqar.younas@milele.com>
2024-09-02 16:04:29 +0400

    Fixing of Export button on Leads

--- app/Imports/VehicleNetSuiteCostImport.php
+++ app/Imports/VehicleNetSuiteCostImport.php
@@ -11,27 +11,32 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 class VehicleNetSuiteCostImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
-    {
-        $vehicle = Vehicles::where('vin', $row['vin'])->first();
+{
+    $vehicle = Vehicles::where('vin', $row['vin'])->first();
 
-        if ($vehicle) {
-            $vehicleId = $vehicle->id;
-            $userId = Auth::id();
-            $existingRecord = VehicleNetsuiteCost::where('vehicles_id', $vehicleId)->first();
-            if ($existingRecord) {
-                $existingRecord->update([
-                    'cost' => $row['cost'],
-                    'netsuite_link' => $row['netsuite_link'],
-                    'created_by' => $userId,
-                ]);
-            } else {
-                return new VehicleNetsuiteCost([
-                    'vehicles_id' => $vehicleId,
-                    'cost' => $row['cost'],
-                    'netsuite_link' => $row['netsuite_link'],
-                    'created_by' => $userId,
-                ]);
-            }
+    if ($vehicle) {
+        $vehicleId = $vehicle->id;
+        $userId = Auth::id();
+        $vin = $row['vin'];
+        
+        // Dynamically create the netsuite_link
+        $netsuiteLink = "https://5676368.app.netsuite.com/app/common/search/searchresults.nl?searchtype=Transaction&BRX_CUSTRECORD_ADVS_VEH_VINtype=IS&BRX_CUSTRECORD_ADVS_VEH_VIN={$vin}&detail=BRX_CUSTRECORD_ADVS_VEH_VIN&detailname={$vin}";
+
+        $existingRecord = VehicleNetsuiteCost::where('vehicles_id', $vehicleId)->first();
+        if ($existingRecord) {
+            $existingRecord->update([
+                'cost' => $row['cost'],
+                'netsuite_link' => $netsuiteLink,
+                'created_by' => $userId,
+            ]);
+        } else {
+            return new VehicleNetsuiteCost([
+                'vehicles_id' => $vehicleId,
+                'cost' => $row['cost'],
+                'netsuite_link' => $netsuiteLink,
+                'created_by' => $userId,
+            ]);
         }
     }
+}
 }
