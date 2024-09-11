b07f5b2f380ce8d9a38bbdd4a9d3d72f133e91e1
Rejitha Rajendran <rejitha.rajendran@milele.com>
2024-09-11 15:05:39 +0400

    documentation status comment getting as null resolved from show details page

--- resources/views/work_order/export_exw/doc_approval.blade.php
+++ resources/views/work_order/export_exw/doc_approval.blade.php
@@ -47,7 +47,7 @@
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary" onclick="submitDocStatus('{{ $workOrder->id }}', '{{ $workOrder->wo_number }}')">Update Status</button>						</div>
+							<button type="button" class="btn btn-primary" onclick="submitDocStatusSingle('{{ $workOrder->id }}', '{{ $workOrder->wo_number }}')">Update Status</button>						</div>
 					</div>
 				</div>
 			</div>
@@ -55,17 +55,17 @@
 	@endif
 @endif
 <script type="text/javascript">
-function submitDocStatus(workOrderId, woNumber) {
+	function submitDocStatusSingle(workOrderId, woNumber) {
 		// Get the selected status
 		const selectedStatus = document.querySelector(`#updateDocStatusModal input[name="docStatus"]:checked`).value;
 
+		// Get the value of the textarea
+		const comment = document.getElementById(`docComment`).value;
 		// Display the confirmation dialog
 		alertify.confirm(
 			'Confirmation Required', // Title of the confirmation dialog
 			`Are you sure you want to update the documentation status for work order ${woNumber} to ${selectedStatus}?`, // Message in the dialog
 			function() { // If the user clicks "OK"
-				const comment = document.getElementById(`docComment`).value;
-
 				// Perform the AJAX request to update the status
 				$.ajax({
 					url: '/update-wo-doc-status',
