d226634f711ef60f120729924b1257e68d0d3fc7
AnuMathew Sunny <anumathew.sunny@milele.com>
2024-10-04 15:05:52 +0400

    removed unwanted feilds using migration in DP module

---
+++ database/migrations/2024_10_04_131525_drop_loi_id_in_pfi_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pfi', function (Blueprint $table) {
+            // $table->dropForeign(['letter_of_indent_id']);         
+            $table->dropColumn('letter_of_indent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pfi', function (Blueprint $table) {
+            $table->bigInteger('letter_of_indent_id');
+        });
+    }
+};
