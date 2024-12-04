9c7bb161e8b803f40788b2c3fcadc525dbff0c73
waqar1944 <waqar.younas@milele.com>
2024-12-04 11:32:45 +0400

    permissions into available stocks

--- resources/views/vehicles/available.blade.php
+++ resources/views/vehicles/available.blade.php
@@ -1035,14 +1035,26 @@ table.dataTable thead th select {
         33: 'warehouse.name',
         34: 'vehicles.territory',
         35: 'countries.name',
-        36: 'costprice',
-        37: 'vehicles.minimum_commission',
-        // 38: 'vehicles.gp',
-        38: 'vehicles.price',
-        39: 'vehicles.ownership_type',
-        40: 'vehicles.custom_inspection_number',
-        41: 'vehicles.custom_inspection_status',
     };
+// Extend columnMap based on permissions
+if (hasManagementPermission) {
+    columnMap[36] = 'costprice';
+    columnMap[37] = 'vehicles.minimum_commission';
+    columnMap[38] = 'vehicles.price';
+    columnMap[39] = 'vehicles.ownership_type';
+    columnMap[40] = 'vehicles.custom_inspection_number';
+    columnMap[41] = 'vehicles.custom_inspection_status';
+} else if (hasPricePermission) {
+    columnMap[36] = 'vehicles.minimum_commission';
+    columnMap[37] = 'vehicles.price';
+    columnMap[38] = 'vehicles.ownership_type';
+    columnMap[39] = 'vehicles.custom_inspection_number';
+    columnMap[40] = 'vehicles.custom_inspection_status';
+} else {
+    columnMap[36] = 'vehicles.ownership_type';
+    columnMap[37] = 'vehicles.custom_inspection_number';
+    columnMap[38] = 'vehicles.custom_inspection_status';
+}
             var table3 = $('#dtBasicExample3').DataTable({
     processing: true,
     serverSide: true,
