282ad3ecaf1f39251610c5bfaef79bd89130ea09
Rejitha Rajendran <rejitha.rajendran@milele.com>
2025-01-06 17:03:05 +0400

    remove success and error msg from blade where no need

--- resources/views/work_order/penalty/cleared.blade.php
+++ resources/views/work_order/penalty/cleared.blade.php
@@ -24,18 +24,6 @@ $hasPermission = Auth::user()->hasPermissionForSelectedRole(['view-vehicle-penal
     </div>
 
     <div class="card-body">
-        @if (Session::has('error'))
-            <div class="alert alert-danger" >
-                <button type="button" class="btn-close p-0 close" data-dismiss="alert">x</button>
-                {{ Session::get('error') }}
-            </div>
-        @endif
-        @if (Session::has('success'))
-            <div class="alert alert-success" id="success-alert">
-                <button type="button" class="btn-close p-0 close" data-dismiss="alert">x</button>
-                {{ Session::get('success') }}
-            </div>
-        @endif
         <div class="row">
             <div class="table-responsive dragscroll">
                 <table class="table table-striped table-editable table-condensed my-datatableclass">
