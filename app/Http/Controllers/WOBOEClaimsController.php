842f5d1e4e8d66ac3a0d74c2887551aac47e3db5
Rejitha Rajendran <rejitha.rajendran@milele.com>
2025-01-07 10:58:42 +0400

    debugging claim listing in test server

--- app/Http/Controllers/WOBOEClaimsController.php
+++ app/Http/Controllers/WOBOEClaimsController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 class WOBOEClaimsController extends Controller
 {
     public function getPendingClaims() { 
-        // try {
+        try {
             $boes = WOBOE::select('id', 'wo_id','boe', 'declaration_number','declaration_date')->with(['claim', 'workOrder']) 
             ->where(function($query) {
                 // Condition 1: No associated claim
@@ -40,16 +40,16 @@ class WOBOEClaimsController extends Controller
             });  
             (new UserActivityController)->createActivity('Open Claim Pending BOE Listing');
             return view('work_order.claims.index', compact('datas'));
-        // } catch (\Exception $e) {
-        //     DB::rollBack(); // Rollback transaction in case of error
-        //     // Log the error
-        //     Log::channel('workorder_error_report')->error('Error fetching claim pending boe information by ' . (Auth::check() ? Auth::user()->name : 'Guest'), [
-        //         'error' => $e->getMessage(),
-        //         'trace' => $e->getTraceAsString(),
-        //     ]);
-        //     // Show a friendly error page
-        //     return response()->view('errors.generic', [], 500); // Return a 500 error page
-        // }
+        } catch (\Exception $e) {
+            DB::rollBack(); // Rollback transaction in case of error
+            // Log the error
+            Log::channel('workorder_error_report')->error('Error fetching claim pending boe information by ' . (Auth::check() ? Auth::user()->name : 'Guest'), [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            // Show a friendly error page
+            return response()->view('errors.generic', [], 500); // Return a 500 error page
+        }
     }
     public function getSubmittedClaims() {
         try {
