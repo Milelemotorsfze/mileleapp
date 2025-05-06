5609841f3e56f54d900457cc8e60eac00f1d72c4
AnuMathew Sunny <anumathew.sunny@milele.com>
2025-05-06 12:51:32 +0400

    sales person added in pfi

--- app/Http/Controllers/ProformaInvoiceController.php
+++ app/Http/Controllers/ProformaInvoiceController.php
@@ -50,7 +50,7 @@ class ProformaInvoiceController extends Controller {
         })->get();
         $sales_persons = ModelHasRoles::with('user')->where('role_id', 7)
                 ->orwhereHas('user', function($query) {
-                    $query->whereIn('id', [17,20,168,40]);
+                    $query->whereIn('id', [17,20,168,40,190]);
                 })
                 ->get();
         $countries = Country::all();
@@ -521,7 +521,7 @@ class ProformaInvoiceController extends Controller {
         $usd_to_eru_rate = Setting::where('key', 'usd_to_euro_convertion_rate')->first();
         $sales_persons = ModelHasRoles::with('user')->where('role_id', 7)
                             ->orwhereHas('user', function($query) {
-                                $query->whereIn('id', [17,20,168,40]);
+                                $query->whereIn('id', [17,20,168,40,190]);
                             })
                             ->get();
 
