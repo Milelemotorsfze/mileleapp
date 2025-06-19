27354378e4e826688ac7805224d4e11e96d42390
hammad8634 <hammadmukhtar1122@gmail.com>
2025-06-19 17:24:06 +0400

    corrected the PO file path in SO Duplicates Page

--- resources/views/so_finalizations/edit.blade.php
+++ resources/views/so_finalizations/edit.blade.php
@@ -355,10 +355,10 @@
                                                 <br>
                                                 <strong>PO Created By:</strong> {{ $vehicle->purchasingOrder->createdBy->name ?? '-' }} |
                                                 <strong>PO Document:</strong>
-                                                <a href="{{ asset('storage/' . $vehicle->purchasingOrder->pl_file_path) }}" target="_blank" class="btn btn-primary btn-sm me-2">
+                                                <a href="{{ asset$vehicle->purchasingOrder->pl_file_path) }}" target="_blank" class="btn btn-primary btn-sm me-2">
                                                     <i class="fa fa-eye"></i> View PO File
                                                 </a>
-                                                <!-- <a href="{{ asset('storage/' . $vehicle->purchasingOrder->pl_file_path) }}" target="_blank">View File</a> -->
+                                                <!-- <a href="{{ asset($vehicle->purchasingOrder->pl_file_path) }}" target="_blank">View File</a> -->
                                                 @endif
                                                 <br />
                                                 <br />
