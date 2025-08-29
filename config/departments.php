9532cb95c3a88115b282e207b4811c8bbf6d3e27
Basharat Ali <basharat.ali@milele.com>
2025-08-29 18:15:02 +0500

    team mails uncommented

--- config/departments.php
+++ config/departments.php
@@ -12,20 +12,20 @@ return [
     */
 
     'estimation_reminders' => [
-        // 'demand_planning' => [
-        //     env('DP_TEAM_EMAIL', 'team.dp@milele.com'),
-        // ],
-        // 'sales_support' => [
-        //     env('SALESUPPORT_TEAM_EMAIL', 'team.salesupport@milele.com'),
-        // ],
-        // 'logistics' => [
-        //     env('LOGISTICS_TEAM_EMAIL', 'team.logistics@milele.com'),
-        // ],
-        // 'warehouse_team' => [
-        //     env('WAREHOUSE_TEAM_EMAIL', 'team.warehouse@milele.com'),
-        // ],
+        'demand_planning' => [
+            env('DP_TEAM_EMAIL', 'team.dp@milele.com'),
+        ],
+        'sales_support' => [
+            env('SALESUPPORT_TEAM_EMAIL', 'team.salesupport@milele.com'),
+        ],
+        'logistics' => [
+            env('LOGISTICS_TEAM_EMAIL', 'team.logistics@milele.com'),
+        ],
+        'warehouse_team' => [
+            env('WAREHOUSE_TEAM_EMAIL', 'team.warehouse@milele.com'),
+        ],
         'qc_team' => [
-            env('QC_TEAM_EMAIL', 'samaviya.riaz@milele.com'),
+            env('QC_TEAM_EMAIL', 'team.qc@milele.com'),
         ],
         'developer' => [
             env('DEVELOPER_EMAIL', 'basharat.ali@milele.com'),
