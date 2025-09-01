367ea4481a48d6f5cbfd09a6e4a6af1786de1ccb
Basharat Ali <basharat.ali@milele.com>
2025-09-01 11:36:56 +0500

    mails code refactored

--- config/departments.php
+++ config/departments.php
@@ -12,21 +12,21 @@ return [
     */
 
     'estimation_reminders' => [
-        'demand_planning' => [
-            env('DP_TEAM_EMAIL', 'team.dp@milele.com'),
-        ],
-        'sales_support' => [
-            env('SALESUPPORT_TEAM_EMAIL', 'team.salesupport@milele.com'),
-        ],
-        'logistics' => [
-            env('LOGISTICS_TEAM_EMAIL', 'team.logistics@milele.com'),
-        ],
-        'warehouse_team' => [
-            env('WAREHOUSE_TEAM_EMAIL', 'team.warehouse@milele.com'),
-        ],
-        'qc_team' => [
-            env('QC_TEAM_EMAIL', 'team.qc@milele.com'),
-        ],
+        // 'demand_planning' => [
+        //     env('DP_TEAM_EMAIL', 'team.dp@milele.com'),
+        // ],
+        // 'sales_support' => [
+        //     env('SALESUPPORT_TEAM_EMAIL', 'team.salesupport@milele.com'),
+        // ],
+        // 'logistics' => [
+        //     env('LOGISTICS_TEAM_EMAIL', 'team.logistics@milele.com'),
+        // ],
+        // 'warehouse_team' => [
+        //     env('WAREHOUSE_TEAM_EMAIL', 'team.warehouse@milele.com'),
+        // ],
+        // 'qc_team' => [
+        //     env('QC_TEAM_EMAIL', 'team.qc@milele.com'),
+        // ],
         'developer' => [
             env('DEVELOPER_EMAIL', 'basharat.ali@milele.com'),
         ],
