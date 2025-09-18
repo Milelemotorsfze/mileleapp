d25eb74c440bb735ad5b429811a27f4e8f91db4c
Basharat Ali <basharat.ali@milele.com>
2025-09-18 18:56:46 +0500

    email notification for dp & operation team revoked

--- config/departments.php
+++ config/departments.php
@@ -36,12 +36,6 @@ return [
         'marketing' => [
             env('MARKETING_TEAM_EMAIL', 'team.marketing@milele.com'),
         ],
-        'operations' => [
-            env('OPERATIONS_TEAM_EMAIL', 'team.operations@milele.com'),
-        ],
-        'demand_planning' => [
-            env('DP_TEAM_EMAIL', 'team.dp@milele.com'),
-        ],
         'sales' => [
             env('SALES_TEAM_EMAIL', 'team.sales@milele.com'),
         ],
